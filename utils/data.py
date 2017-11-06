0763d84e41e75713e6a25c1844b0518d8dae1962
DiegoAgher <diego.aguado@wizeline.com>
2017-11-05 23:22:32 -0600

    compose correctly the path to read files

--- utils/data.py
+++ utils/data.py
@@ -5,27 +5,27 @@ import pandas as pd
 def get_IMDB_data():
     train_data = get_train_data()
     test_data = get_test_data()
-    
+
     return train_data, test_data
 
 
 def get_train_data():
-    data_path = '/data/aclImdb/train'
+    data_path = 'data/aclImdb/train'
     return _get_parsed_data_from_reviews(data_path)
 
 
 def get_test_data():
-    data_path = '/data/aclImdb/test'
+    data_path = 'data/aclImdb/test'
     return _get_parsed_data_from_reviews(data_path)
 
 
 def _get_parsed_data_from_reviews(data_path):
-    train_pos = data_path + '/pos'
-    train_neg = data_path + '/neg'
+    positive_path = data_path + '/pos'
+    negative_path = data_path + '/neg'
 
     data = pd.DataFrame(columns=['id', 'sentiment', 'text', 'rating'])
-    data = _parse_reviews_from_text_file(train_pos, data)
-    data = _parse_reviews_from_text_file(train_neg, data)
+    data = _parse_reviews_from_text_file(positive_path, data)
+    data = _parse_reviews_from_text_file(negative_path, data)
 
     data.reset_index(inplace=True, drop=True)
     return data.sample(frac=1)
@@ -35,7 +35,7 @@ def _parse_reviews_from_text_file(reviews_path, dataframe):
     sentiment = reviews_path.split('/')[-1]
     files_list = os.listdir(reviews_path)
     for enum, file_name in enumerate(files_list):
-        path_to_file = reviews_path + file_name
+        path_to_file = reviews_path + '/' + file_name
         with open(path_to_file, 'rb') as file:
             review_text = (file.readlines()[0]).decode('utf-8')
             review_text = review_text.replace('<br />', ' ')
