33f6784991a4f4d082102f25bf51b1811529fd02
DiegoAgher <diego.aguado@wizeline.com>
2017-11-08 22:38:42 -0600

    fix formating of output string

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -6,16 +6,21 @@ from utils.tf_graphs import sequence2class_lstm_inference_graph
 
 class SentimentAnalyzer(object):
     def __init__(self, model, embedding_matrix):
-        with tf.Graph.as_default() as graph:
-            self.graph_elements = model(batch_size=1, num_layers=2, num_classes=2,
+        graph = tf.Graph()
+        with graph.as_default() as g:
+            self.graph_elements = model(batch_size=1, num_layers=2,
+                                        num_classes=2,
                                         embedding_matrix=embedding_matrix)
             self.saver = tf.train.Saver()
-            self.graph = graph
+            self.graph = g
             self.positive_encoding = np.array([1, 0])
             self.negative_encoding = np.array([0, 1])
 
     def predict_text(self, text):
-        mapped_text, _ = build_embedding_ids_sequences(text)
+        mapped_text_dict, _ = build_embedding_ids_sequences(text)
+        embedding_sequence = mapped_text_dict[0]
+        mapped_text = np.reshape(embedding_sequence,
+                                 (1,len(embedding_sequence)))
         inference_dict = {
             self.graph_elements['input_sequence']: mapped_text
                          }
@@ -23,12 +28,14 @@ class SentimentAnalyzer(object):
         with tf.Session(graph=self.graph) as sess:
             self.saver.restore(sess=sess, save_path='sentiment_analysis_tsm')
 
-            probabilities = sess.run(g['predictions'], inference_dict)
-            predictions = np.argmax(probabilities)
-            print(predictions)
-            if predictions == self.positive_encoding:
-                print("Text: ", text, """ has a positive feeling with
-                 probability """, probabilities[np.argmax(probabilities)])
+            probabilities = sess.run(self.graph_elements['predictions'],
+                                     inference_dict)
+            prediction = np.argmax(probabilities)
+            feeling_prob = probabilities[0, prediction]
+            print("probabilities: ", probabilities)
+            feeling = 'positive' if prediction == 0 else 'negative'
+            print("""The text {0} has a {1} feeling with probability {2}""".
+                  format(text, feeling, feeling_prob))
 
 
 Analyzer = SentimentAnalyzer(sequence2class_lstm_inference_graph,
